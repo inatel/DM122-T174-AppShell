b4b2620651d94bb5d54c441b7626db43481dd662
Edy Segura <edysegura@gmail.com>
2020-09-19 15:29:18 -0300

    Add first offline experience

--- sw.js
+++ sw.js
@@ -1,6 +1,16 @@
+const cacheName = 'app-shell-v1';
+const assetsToCache = [
+  'offline.html'
+];
+
+async function cacheStaticAssets() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(assetsToCache);
+}
 
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing Service Worker...', event);
+  event.waitUntil(cacheStaticAssets());
   self.skipWaiting();
 });
 
@@ -9,6 +19,16 @@ self.addEventListener('activate', event => {
   return self.clients.claim();
 });
 
+async function networkFirst(request) {
+  try {
+    return await fetch(request);
+  } catch (error) {
+    const cache = await caches.open(cacheName);
+    return cache.match('offline.html');
+  }
+}
+
 self.addEventListener('fetch', event => {
   console.log('[Service Worker] Fetch event: ' + event.request.url);
+  event.respondWith(networkFirst(event.request));
 });
